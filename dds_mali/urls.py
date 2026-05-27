7c52c870003c50b97404ceba929d74fc2ebb7ee8
dioti-dev <120653209+tidiani-max@users.noreply.github.com>
2026-05-27 21:53:22 +0700

    Update urls.py

--- dds_mali/urls.py
+++ dds_mali/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path  # Added re_path here
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve           # Added serve here
 from django.contrib.sitemaps.views import sitemap
 from core.sitemaps import StaticViewSitemap, WebinarSitemap, ScholarshipSitemap
 
@@ -23,4 +24,12 @@ urlpatterns = [
     path('webinars/', include('webinars.urls')),
     path('contact/', include('contact.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# This is your ultimate safety shield block for media files in production
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
