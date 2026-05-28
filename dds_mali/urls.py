0803fefe272e1c2cd2c0f3d6d86d1c7fa17038f0
dioti-dev <120653209+tidiani-max@users.noreply.github.com>
2026-05-29 00:01:14 +0700

    Update urls.py

--- dds_mali/urls.py
+++ dds_mali/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include, re_path  # Added re_path here
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve           # Added serve here
+from django.views.static import serve
 from django.contrib.sitemaps.views import sitemap
 from core.sitemaps import StaticViewSitemap, WebinarSitemap, ScholarshipSitemap
 
@@ -26,7 +26,7 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 
-# This is your ultimate safety shield block for media files in production
+# Serve media assets explicitly regardless of DEBUG state
 if not settings.DEBUG:
     urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
